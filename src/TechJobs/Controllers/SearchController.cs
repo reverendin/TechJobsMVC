5aa7c28309de3de097242476d1c9eefd22e28643
Eric Hegeman <miracleofmagick@gmail.com>
2018-02-22 22:31:48 -0500

    completed

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
+using System.Globalization;
+using System.Threading;
 
 namespace TechJobs.Controllers
 {
@@ -15,6 +17,22 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+
+            if(searchTerm != null)
+            {
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+                ViewBag.searchTerm = searchTerm;
+            }
+            else
+            {
+                return Redirect("/search");
+            }
+
+            return View("Index");
+        }
 
     }
 }
---
+++ src/TechJobs/Models/StringExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobs.Models
+{
+    public static class StringExtensions
+    {
+        public static string ToTitleCase(this string str)
+        {
+            var tokens = str.Split(new[] { " ", "-" }, StringSplitOptions.RemoveEmptyEntries);
+            for (var i = 0; i < tokens.Length; i++)
+            {
+                var token = tokens[i];
+                tokens[i] = token == token.ToUpper()
+                    ? token
+                    : token.Substring(0, 1).ToUpper() + token.Substring(1).ToLower();
+            }
+            return string.Join(" ", tokens);
+        }
+    }
+}
